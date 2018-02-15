fabf22e10eac14412e539f67ef6b00c0a6632aa2
reinharn <nate.reinhardt@wwt.com>
2018-02-15 10:20:56 -0600

    removing modules and adnriod stoof

--- node_modules/nativescript-dev-sass/lib/watch.js
+++
@@ -1,5 +0,0 @@
-var converter = require('./converter');
-
-module.exports = function (logger, projectData, usbLiveSyncService) {
-	return converter.convert(logger, projectData.projectDir, { watch: true });
-}
--- platforms/android/app/src/main/assets/app/tns_modules/nativescript-pro-ui/scripts/eula-prompt.js
+++
@@ -1,1 +0,0 @@
-console.log("Progress NativeScript Pro UI is licensed under the End User License Agreement for NativeScript Pro UI: https://github.com/telerik/nativescript-ui-feedback/blob/master/LICENSE.md");
